349200562239ea1e10c9ed379f02b76dd427e8ec
Paco Martín Fernández <fmartinfdez@gmail.com>
2017-03-03 18:26:58 +0000

    Version 1.0.0

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 setup(name = 'IBMQuantumExperience',
       packages = ['IBMQuantumExperience'], # this must be the same as the name above
       version='1.0.0',
-      author='IBM Research Emergent Solutions',
+      author='IBM Research ETX',
       description='A Python library for the Quantum Experience API.',
       author_email='fmartinfdez@gmail.com',
-      url = 'https://github.com/IBMQuantum/qiskit-api-py',
+      url = 'https://github.com/IBM/qiskit-api-py',
       keywords = ['ibm', 'quantum computer', 'quantum experience'],
       license='MIT',
       install_requires=[
